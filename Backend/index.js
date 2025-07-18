0421017b319bb53f08653582e2c44fcad2e36827
arpitsingh0311 <arpit031104@gmail.com>
2025-07-18 14:36:30 +0530

    final

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 const app = express();
@@ -32,8 +33,15 @@ app.use("/api/company",companyRoute);
 app.use("/api/job",jobRoute);
 app.use("/api/application",applicationRoute);
 
+// ------------ Code For Deployment -------------
 
-
+if(process.env.NODE_ENV === "production") {
+  const dirpath = path.resolve();
+  app.use(express.static('./Frontend/dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(dirpath, './Frontend/dist', 'index.html'));
+  });
+}
 
 
 app.listen(PORT, () => {
